2b521350dc69514dbc960e77faed8b9576140da7
YaminiMoturu <moturuyamini@gmail.com>
2019-08-26 21:38:32 -0700

    add

---
+++ raptor/absolute.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? num;
+
+   raptor_prompt_variable_zzyz ="Enter the number: ";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> num;
+
+   return 0;
+}
